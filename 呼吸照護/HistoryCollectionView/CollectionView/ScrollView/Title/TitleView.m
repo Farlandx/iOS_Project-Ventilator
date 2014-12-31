ac580fb8c6b9b9601cb0b71f0cc3af199a59f258
Farlandx <farlandx@gmail.com>
2014-12-31 14:17:53 +0800

    Add history sidebar title.

--- 呼吸照護/HistoryCollectionView/CollectionView/ScrollView/Title/TitleView.m
+++ 呼吸照護/HistoryCollectionView/CollectionView/ScrollView/Title/TitleView.m
@@ -10,6 +10,7 @@
 
 #import "HistoryCollectionViewController.h"
 #import "TitleLabel.h"
+#import "TitleSidebar.h"
 
 @interface TitleView ()
 @property (nonatomic, strong) NSArray *titleArray;
@@ -35,7 +36,7 @@
         CGFloat y = 0;
         
         for (int i = 0; i < [titleArray count];i++) {
-            TitleLabel *label = [[TitleLabel alloc] initWithFrame:CGRectMake(0, y, self.frame.size.width, HEADER_HEIGHT)];
+            TitleLabel *label = [[TitleLabel alloc] initWithFrame:CGRectMake(SIDEBAR_TITLE_WIDTH, y, self.frame.size.width - SIDEBAR_TITLE_WIDTH, HEADER_HEIGHT)];
             [label setText:[titleArray objectAtIndex:i]];
             
             [self addSubview:label];
@@ -44,18 +45,33 @@
         }
         
         totalHeight = y;
+        
+        [self drawSidebarTitle];
     }
     
     return self;
 }
 
-/*
-// Only override drawRect: if you perform custom drawing.
-// An empty implementation adversely affects performance during animation.
-- (void)drawRect:(CGRect)rect
-{
-    // Drawing code
+- (void)drawSidebarTitle {
+    
+    NSArray *sidebarAry = @[@{@"lines":[NSNumber numberWithInt:10], @"text":@"呼吸器之使用"},
+                            @{@"lines":[NSNumber numberWithInt:6], @"text":@"動脈血氣體分析"},
+                            @{@"lines":[NSNumber numberWithInt:6], @"text":@"呼吸及氣道監視"},
+                            @{@"lines":[NSNumber numberWithInt:4], @"text":@"血行力學"},
+                            @{@"lines":[NSNumber numberWithInt:5], @"text":@"血液生化"}];
+    
+    CGFloat posY = 0.0f;
+    
+    for (int i = 0; i < sidebarAry.count; i++) {
+        TitleSidebar *sidebar = [[TitleSidebar alloc] initWithFrame:CGRectMake(0,
+                                                                               posY,
+                                                                               SIDEBAR_TITLE_WIDTH,
+                                                                               HEADER_HEIGHT * [[sidebarAry[i] objectForKey:@"lines"] intValue])
+                                                               text:[sidebarAry[i] objectForKey:@"text"]];
+        [self addSubview:sidebar];
+        posY += sidebar.frame.size.height;
+    }
+    
 }
-*/
 
 @end
--- 呼吸照護/HistoryCollectionView/HistoryCollectionViewController.h
+++ 呼吸照護/HistoryCollectionView/HistoryCollectionViewController.h
@@ -9,8 +9,9 @@
 #import <UIKit/UIKit.h>
 #import "DtoVentExchangeUploadBatch.h"
 
-#define HEADER_HEIGHT 25
-#define HEADER_WIDTH 200
+#define HEADER_HEIGHT 25.0
+#define HEADER_WIDTH 200.0
+#define SIDEBAR_TITLE_WIDTH 25.0
 #define ARRAY_TITLE @[@"Mode", @"T.V set/total", @"Rate set/total", @"Flow", @"M.V set/total", @"Insp. T / I:E", @"FiO2", @"Peak/Plateau", @"Mean/PEEP", @"P.S/P.C", @"PH", @"PaCO2", @"PaO2", @"HCO3/B.E", @"SaO2/SpO2", @"PA-aO2/Shunt", @"RR", @"T.V/M.V", @"Pimax", @"E.T siz/mark", @"Cuff Pressure", @"Breathing Sound", @"HR", @"BP", @"I/O", @"Conscious Level", @"Hb/Sugar", @"Na/K", @"Ca/Mg", @"BUN/Cr", @"Albumin/CI"]
 
 @interface HistoryCollectionViewController : UIViewController
